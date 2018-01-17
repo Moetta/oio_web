7e9021cd2858dfaa6e6fa8ef40904697a742bcea
Moetta <matteo.gibe@gmail.com>
2018-01-17 10:19:57 +0100

    File input verifications
    File size
    File dimensions

--- public/libs/js/app.js
+++ public/libs/js/app.js
@@ -37,21 +37,50 @@
 	var DELETE = mkREST('DELETE');
 	
 	// Encodes an image file to base 64 and outputs the result in a input field.
-	var ToB64 = function (element, output) {
-		var file = element.files[0],
-			reader = new FileReader();
+	var ToB64 = function(file, output) {
+		var reader = new FileReader();
 		reader.onloadend = function() {
 			output.val(reader.result);
 		}
 		reader.readAsDataURL(file);
 	};
 	
+	// On file input, clears previous stored base64, then checks before encoding it.
+	var fileInput = function(file, output, msg) {
+		output.val('');
+
+		var fileSize = file.size;
+		if (fileSize <= Math.pow(2,20)) {
+			var img = new Image(),
+				imgwidth = 0,
+				imgheight = 0,
+				maxwidth = 1920,
+				maxheight = 1080;
+			img.src = window.URL.createObjectURL(file);
+			img.onload = function() {
+				imgwidth = this.width;
+				imgheight = this.height;
+				if (imgwidth <= maxwidth && imgheight <= maxheight) {
+					ToB64(file, output);
+					msg.html('<span class="green-text text-darken-2">Image ok.</span>');
+				}
+				else {
+					msg.html('<span class="red-text text-darken-2">La taille de l\'image dépasse les ' + maxwidth + 'x' + maxheight + ' pixels.</span>');
+				}
+			}
+		}
+		else {
+			msg.html('<span class="red-text text-darken-2">Le poids de l\'image dépasse les 1Mo.</span>');
+		}
+	}
+	
 	exports.app = {
 		GET: GET,
 		PUT: PUT,
 		POST: POST,
 		DELETE: DELETE,
-		encodeFileToB64: ToB64
+		encodeFileToB64: ToB64,
+		checkFileAndEncode: fileInput
 	};
 
 })(this);
